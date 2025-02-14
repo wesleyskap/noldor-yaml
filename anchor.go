7c72edcd3856a887e940d433d3580c50d536534c
wesleyskap <wesleyskap@gmail.com>
2025-02-14 14:22:45 -0300

    feat: implement NewAnchorRegistry constructor

--- anchor.go
+++ anchor.go
@@ -4,3 +4,12 @@
 type AnchorRegistry struct {
 	anchors map[string]*Node
 }
+
+// NewAnchorRegistry creates a new anchor table.
+// Usage example:
+//   reg := NewAnchorRegistry()
+func NewAnchorRegistry() *AnchorRegistry {
+	return &AnchorRegistry{
+		anchors: make(map[string]*Node),
+	}
+}
