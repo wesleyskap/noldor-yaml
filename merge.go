427f23c9b323b29e9b1051d129de5d4e1238c995
wesleyskap <wesleyskap@gmail.com>
2025-03-11 15:55:04 -0300

    feat: detect << merge key tokens in mapping content pairs

--- merge.go
+++ merge.go
@@ -14,5 +14,11 @@ func (m *MergeKeyResolver) ApplyMerge(mapping *Node) error {
 	if mapping == nil || mapping.Kind != MappingNode {
 		return fmt.Errorf("yaml: merge key can only be applied to MappingNode, got %v", mapping)
 	}
+	for i := 0; i < len(mapping.Content); i += 2 {
+		keyNode := mapping.Content[i]
+		if keyNode.Value == "<<" {
+			// Found merge key token
+		}
+	}
 	return nil
 }
