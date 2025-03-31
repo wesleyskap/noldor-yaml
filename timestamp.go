f30b19e1cb6a3d7b00438f5f24fccd9bff74d9e3
wesleyskap <wesleyskap@gmail.com>
2025-03-31 14:30:40 -0300

    feat: add RFC3339 format parsing support

--- timestamp.go
+++ timestamp.go
@@ -1,8 +1,9 @@
 ﻿package yaml
 
-import "time"
+import (
+	"time"
+)
 
-// ParseTimestamp parses standard ISO-8601 and YAML 1.1/1.2 timestamp scalar representations.
 func ParseTimestamp(input string) (time.Time, error) {
-	return time.Time{}, nil
+	return time.Parse(time.RFC3339, input)
 }
